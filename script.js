db8d795c497edc2f9424f9fa8fb3fd880cb0b460
Lívia Correa Goltz <liviacorreagoltz@gmail.com>
2025-08-20 15:03:42 -0300

    ...
    ......

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const currentPlayer = document.querySelector("currentPlayer");
+const currentPlayer = document.querySelector(".currentPlayer");
 
 let selected;
-let player="x";
+let player="X";
 
 let positions = [
    [1,2,3],
@@ -17,7 +17,7 @@ let positions = [
 function init(){
     selected = [];
 
-    currentPlayer.innerHTML = 'JOGADOR DA VEZ: ${player}';
+    currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
 
     document.querySelectorAll(".game button").forEach((item) => {
        item.innerHTML = "";
@@ -35,6 +35,28 @@ function newMove(e){
         check();
          },[100]);
 
-         player = player === "x" ? "o" : "x";
-         currentPlayer.innerHTML = 'JOGADOR DA VEZ: ${player}';
+         player = player === "X" ? "O" : "X";
+         currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
 }
+
+function check() {
+    let playerLastMove = player === "X" ? "O" : "X";
+
+    const items = selected 
+    .map((item, i) => [item, i])
+    .filter((item) => item[0] === playerLastMove)
+    .map ((item => item [1]));
+
+for (pos of positions){
+    if (pos.every((item) => items.includes(item))) {
+        alert(" O JOGADOR '" + playerLastMove + "' GANHOU!");
+        init();
+        return;
+     }
+  }
+  if (selected.filter((item)=> item).length === 9){
+    alert("DEU IMPATE!");
+    init();
+    return;
+  }
+} 
